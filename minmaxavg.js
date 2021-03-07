9b65afd76bb09cee1020554b34972a60fee527ec
Syed Ali Rabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-03-07 18:21:26 +0800

    Update minmaxavg.js

--- minmaxavg.js
+++ minmaxavg.js
@@ -1,34 +1,35 @@
+//---------------------------------------------
+//Create a function minMaxAvg that will take an array as parameter and will return the smallest number within the array, max number within the array and the average.
+//-------------------------------------------
+
 function minMaxAvg(arrNum){
-
-//initiliaze with zero
-var sum = 0;
-var min = 0;
-var max = 0; 
-var avg = 0
-
-//calculate average
-    
-for(var i = 0; i < arrNum.length; i++){
-  sum+=arrNum[i];
-}
-   avg = sum/arrNum.length 
-    
-//calculate max
-for(var i = 0, i < arrNum.length; i++){
-  if (arrNum[i]>max){
-    max = arrNum[i];
-  }
-}    
- //calculate min
-  for(var i = 0; i<arrNum.length; i++){
-    if(arrNum[i]<min){
-      max = arrNum[i];
+  var sum = 0;
+  var min = arrNum[0];
+  var max = arrNum[0];
+  var avg = arrNum[0];
+  
+  //calc min
+  for( var i = 0; i<arrNum.length; i++){
+    if (arrNum[i]<min){
+      min = 'min is = ' +arrNum[i];
     }
   }
   
-
-  return (min,max,avg)
+  //calc max
+  for (var i = 0; i<arrNum.length; i++){
+    if (arrNum[i]>max){
+      max = 'max is = ' +arrNum[i];
+    }
+  }
+    
+  //calc avg
+  for (var i = 0; i<arrNum.length; i++){
+    sum += arrNum[i];
+     avg = 'avg is = ' + sum/arrNum.length;
+  }
+   
+  
+ return [min, max, avg] ;
 }
 
 console.log(minMaxAvg([3,5,1,9,10,3,2]))
-
